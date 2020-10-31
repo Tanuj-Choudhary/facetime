c6e41e7b231ea4b602cf5b390b8c059ff7245a64
Tanuj <tanujchoudhary001@gmail.com>
2020-10-31 16:39:39 +0530

    Added header to home component

--- client/src/components/home/HomeView.jsx
+++ client/src/components/home/HomeView.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Header from '../header';
 import './homeView.css';
 
 function HomeView() {
     return (
         <div>
-            HomeView      
+            <Header />    
         </div>
     )
 }
