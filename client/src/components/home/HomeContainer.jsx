7acfc6c767cfb8f26a6aae57f1982e9ba62fb6b7
Tanuj <tanujchoudhary001@gmail.com>
2020-10-31 16:23:38 +0530

    Added home component

---
+++ client/src/components/home/HomeContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import HomeView from './HomeView';
+
+function HomeContainer() {
+    return <HomeView />
+}
+
+export default HomeContainer;
---
+++ client/src/components/home/HomeView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homeView.css';
+
+function HomeView() {
+    return (
+        <div>
+            HomeView      
+        </div>
+    )
+}
+
+export default HomeView;
