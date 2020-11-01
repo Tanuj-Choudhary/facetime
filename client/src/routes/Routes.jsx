d9a03f9441c9dfe4255ca137621cb39b3c25f500
Tanuj <tanujchoudhary001@gmail.com>
2020-11-01 11:19:38 +0530

    Structured whole code with prettier

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -3,15 +3,15 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from '../components/home';
 
 function Routes() {
-    return (
-        <>
-            <HashRouter>
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                </Switch>
-            </HashRouter>   
-        </>
-    )
+  return (
+    <>
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </HashRouter>
+    </>
+  );
 }
 
 export default Routes;
