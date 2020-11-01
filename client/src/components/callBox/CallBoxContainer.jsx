af1ee1b7fd40952c34c562d196cf4d4c7bab5813
Tanuj <tanujchoudhary001@gmail.com>
2020-11-01 23:08:22 +0530

    Added callbox container component

---
+++ client/src/components/callBox/CallBoxContainer.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import CallBoxView from './CallBoxView';
+
+function CallBoxContainer() {
+  const [fields, setfields] = useState({ friendID: '' });
+  const [isModalOpen, setisModalOpen] = useState(false);
+
+  const history = useHistory();
+
+  // Handling call button
+  const handleClick = (e) => {
+    e.preventDefault();
+    const { friendID } = fields;
+
+    // Empty id show modal
+    if (friendID === '') {
+      setisModalOpen(true);
+      return;
+    }
+
+    // Valid ID Go to rooms/:id
+    history.push(`rooms/${friendID}`);
+  };
+
+  // Controlling input
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const newFields = { ...fields, [name]: value };
+
+    setfields(newFields);
+  };
+
+  const handleModalClose = () => {
+    setisModalOpen(false);
+  };
+
+  return (
+    <CallBoxView
+      fields={fields}
+      handleChange={handleChange}
+      handleClick={handleClick}
+      isModalOpen={isModalOpen}
+      handleModalClose={handleModalClose}
+    />
+  );
+}
+
+export default CallBoxContainer;
