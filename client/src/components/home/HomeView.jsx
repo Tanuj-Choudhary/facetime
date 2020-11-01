2088c071ef12a20d475d970afae26971b17f12bf
Tanuj <tanujchoudhary001@gmail.com>
2020-11-01 14:57:52 +0530

    Added hero component to home view

--- client/src/components/home/HomeView.jsx
+++ client/src/components/home/HomeView.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../../views/header';
+import Hero from '../../views/hero';
 import './homeView.css';
 
 function HomeView() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
