cab23532e01e69d81bdc7ad5cb923acd3e068dc8
Tanuj <tanujchoudhary001@gmail.com>
2020-11-01 23:10:22 +0530

    Handling start and join button logic in home container

--- client/src/components/home/HomeContainer.jsx
+++ client/src/components/home/HomeContainer.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 import HomeView from './HomeView';
 
 function HomeContainer() {
-  return <HomeView />;
+  const [buttonClicked, setbuttonClicked] = useState(null);
+  const roomID = uuidv4();
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    const id = e.target.closest('.btn').id;
+
+    // Start button clicked
+    if (id === 'start') {
+      history.push(`rooms/${roomID}`);
+    }
+
+    // Join button clicked
+    setbuttonClicked(id);
+  };
+
+  return <HomeView handleClick={handleClick} buttonClicked={buttonClicked} />;
 }
 
 export default HomeContainer;
