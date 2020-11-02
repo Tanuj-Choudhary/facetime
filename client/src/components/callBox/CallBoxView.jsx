aae18b23905f4f3b84b71bc69bf795c45e0f11b6
Tanuj <tanujchoudhary001@gmail.com>
2020-11-02 11:59:41 +0530

    Added description to callbox

--- client/src/components/callBox/CallBoxView.jsx
+++ client/src/components/callBox/CallBoxView.jsx
@@ -5,24 +5,31 @@ import './callBox.css';
 
 function CallBoxView({ fields, handleChange, handleClick, isModalOpen, handleModalClose }) {
   return (
-    <div className="callBox">
-      {isModalOpen && (
-        <Modal handleModalAction={handleModalClose}>
-          <h1 className="callbox__modal">Please enter a ID</h1>
-        </Modal>
-      )}
-      <input
-        name="friendID"
-        value={fields.friendID}
-        onChange={handleChange}
-        placeholder="Friend ID"
-        type="text"
-        className="input__id"
-      />
-      <button onClick={handleClick} className="call-btn">
-        Call
-      </button>
-    </div>
+    <>
+      <div className="callBox">
+        {isModalOpen && (
+          <Modal handleModalAction={handleModalClose}>
+            <h1 className="callbox__modal">Please enter a ID</h1>
+          </Modal>
+        )}
+        <input
+          name="friendID"
+          value={fields.friendID}
+          onChange={handleChange}
+          placeholder="Friend ID"
+          type="text"
+          className="input__id"
+        />
+        <button onClick={handleClick} className="call-btn">
+          Call
+        </button>
+      </div>
+
+      <div className="callbox__text">
+        To call your friend, ask them to open <span>facetime</span> in their browser.
+        <br /> Send your ID and wait for their call <span>OR</span> enter their ID and hit call!
+      </div>
+    </>
   );
 }
 
