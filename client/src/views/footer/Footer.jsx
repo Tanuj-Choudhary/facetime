a36877c5b5f600e79f0d71f8ca294a14385d0c77
Tanuj <tanujchoudhary001@gmail.com>
2020-11-02 12:21:03 +0530

    Changed small styling in footer

--- client/src/views/footer/Footer.jsx
+++ client/src/views/footer/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
     <div className="footer">
       <div className="footer__content">
         <div className="footer__info">
-          You need to allow access to video and audio to place calls.Facetime is fully Open Source
-          and does not store any data on its servers.
+          You need to allow access to video and audio to place calls. <br /> Facetime is fully Open
+          Source and does not store any data on its servers.
         </div>
         <div className="footer__credits">Made with ❤️ by Tanuj Choudhary</div>
       </div>
